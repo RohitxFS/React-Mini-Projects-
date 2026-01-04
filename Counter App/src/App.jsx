3320aed48fad41787841a9bfa126be1ccf2c6065
rohitxfs <patilrohitsanjay123@gmail.com>
2026-01-04 19:53:53 +0530

    Mini Projects

---
+++ Counter App/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const App = () => {
+
+  const [num, setNum] = useState(0)
+
+  function increaseNum() {
+    setNum(num + 1)
+  }
+
+  function decreaseNum() {
+    setNum(num - 1)
+  }
+
+  function jumpBy5() {
+    setNum(num + 5 )
+  }
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>Increase</button>
+      <button onClick={decreaseNum}>Decrease</button>
+      <button onClick={jumpBy5}>Increase by 5</button>
+    </div>
+  )
+}
+
+export default App
